dd89b98e3f5a5ebc365a6f49833bff49797396ed
ElenaVonder <evonder13@gmail.com>
2021-04-11 16:09:33 -0400

    added class

---
+++ BankingPractice/Checking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingPractice
+{
+    class Checking
+    {
+
+        public Checking()
+        {
+
+        }
+    }
+}
